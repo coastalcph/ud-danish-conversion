cb088f6b420249661a69d2bfbf2d8ea0ac61cb88
Barbara Plank <bplank@gmail.com>
2015-09-12 20:06:11 +0200

    use preprocess (not conll-x data, as it contains >< in labels); add step-wise data

--- parse_it/run-mst.sh
+++ parse_it/run-mst.sh
@@ -1,3 +1,6 @@
-nohup sh parse-mst.sh ../treebanks/UD_Danish/da-ud-train.conllu ../treebanks/UD_Danish/da-ud-test.conllu ../treebanks/UD_Danish/da-ud-dev.conllu UD_Danish > nohup.mst.out 2> nohup.mst.err & 
+#nohup sh parse-mst.sh ../treebanks/UD_Danish/da-ud-train.conllu ../treebanks/UD_Danish/da-ud-test.conllu ../treebanks/UD_Danish/da-ud-dev.conllu UD_Danish > nohup.mst.out 2> nohup.mst.err & 
 
-nohup sh parse-mst.sh ../data/conll-x/danish_ddt_train.conll ../data/conll-x/danish_ddt_test.conll ../data/conll-x/danish_ddt_dev.conll conll-x > nohup.mst.conll.out 2> nohup.mst.conll.err & 
+
+nohup sh parse-mst.sh ../data/steps/preprocess/danish_ddt_train.conll ../data/steps/preprocess/danish_ddt_test.conll ../data/steps/preprocess/danish_ddt_dev.conll preprocess > nohup.mst.conll.out 2> nohup.mst.conll.err & 
+## do not use conll-x directly, it contains <> in labels!! nohup sh parse-mst.sh ../data/conll-x/danish_ddt_train.conll ../data/conll-x/danish_ddt_test.conll ../data/conll-x/danish_ddt_dev.conll conll-x > nohup.mst.conll.out 2> nohup.mst.conll.err & 
+
